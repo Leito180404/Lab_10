e55249a6ce414fac9f85093f2b1e507181868428
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-12 21:17:06 -0500

    Create BNode.java

---
+++ Actividades/Btree/BNode.java
@@ -0,0 +1,53 @@
+package Actividades.Btree;
+import java.util.ArrayList;
+
+public class BNode<E extends Comparable<E>> {
+    protected ArrayList<E> keys;
+    protected ArrayList<BNode<E>> childs;
+    protected int count;
+    protected int idNode;
+    private static int nodeCounter = 0;
+
+    public BNode(int n) {
+        this.keys = new ArrayList<>(n);
+        this.childs = new ArrayList<>(n);
+        this.count = 0;
+        this.idNode = nodeCounter++;
+        for (int i = 0; i < n; i++) {
+            this.keys.add(null);
+            this.childs.add(null);
+        }
+    }
+
+    public boolean nodeFull(int maxKeys) {
+        return count == maxKeys;
+    }
+
+    public boolean nodeEmpty() {
+        return count == 0;
+    }
+
+    public boolean searchNode(E key, int[] pos) {
+        pos[0] = 0;
+        while (pos[0] < count && key.compareTo(keys.get(pos[0])) > 0) {
+            pos[0]++;
+        }
+        if (pos[0] < count && key.compareTo(keys.get(pos[0])) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[idNode=").append(idNode).append(", keys=");
+        for (int i = 0; i < count; i++) {
+            sb.append(keys.get(i));
+            if (i < count - 1) sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
