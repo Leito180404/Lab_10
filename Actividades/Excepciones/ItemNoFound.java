173d84d2993fef54d77378a691ef5ade0994be1e
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-12 21:17:15 -0500

    Create ItemNoFound.java

---
+++ Actividades/Excepciones/ItemNoFound.java
@@ -0,0 +1,11 @@
+package Excepciones;
+
+public class ItemNoFound extends Exception {
+    public ItemNoFound(String msg) {
+        super(msg);
+    }
+
+    public ItemNoFound() {
+        super("El dato no se encontro en el B-Tree");
+    }
+}
