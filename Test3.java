cf6333c2626ae55596208ef6d07719728dbcf834
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-18 14:47:56 -0500

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -12,7 +12,7 @@ public class Test3 {
 
         try {
             BTree<Integer> arbol = BTree.building_Btree(ruta);
-            System.out.println("\n\u2728 \u00c1rbol B construido con éxito desde archivo:");
+            System.out.println("Arbol B construido con éxito desde archivo:");
             System.out.println(arbol);
         } catch (ItemNoFound e) {
             System.err.println("\n\u274C Error al construir el árbol: " + e.getMessage());
