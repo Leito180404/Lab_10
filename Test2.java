ae997e099c30a607ad5e13bd22c8fe778229f63b
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-06-20 21:04:46 -0500

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -3,35 +3,25 @@ import Btree.BTree;
 
 public class Test2 {
     public static void main(String[] args) {
-        BTree<Integer> btree = new BTree<>(4);
+        BTree<Integer> btree = new BTree<>(5); // Orden 5 como en la actividad
 
-        int[] claves = {
-            31,
-            12, 19,
-            3, 10,
-            13, 16,
-            22, 25, 28,
-            41, 57, 63,
-            33, 35, 40,
-            49, 52, 55,
-            60, 62,
-            67, 70, 72
-        };
+        // Insertar las claves del árbol final
+        int[] claves = {100, 50, 20, 70, 10, 30, 80, 90, 200, 25, 15, 5, 65, 35, 60, 18, 93, 94};
 
         for (int clave : claves) {
             btree.insert(clave);
         }
 
-        System.out.println("\narbol antes de eliminar:");
+        System.out.println("\nÁrbol antes de eliminar:");
         System.out.println(btree);
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nIngresa una clave para eliminar del arbol:");
+        System.out.println("\nIngresa una clave para eliminar del árbol:");
         int eliminar = sc.nextInt();
 
         btree.remove(eliminar);
 
-        System.out.println("\narbol despues de eliminar:");
+        System.out.println("\nÁrbol después de eliminar:");
         System.out.println(btree);
         sc.close();
     }
